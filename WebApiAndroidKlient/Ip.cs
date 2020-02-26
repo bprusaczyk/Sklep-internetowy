fe9d685d1a443b609003a019437819fe8af220ee
Bartosz Prusaczyk <37440663+bprusaczyk@users.noreply.github.com>
2020-02-26 17:53:20 +0100

    Create Ip.cs

---
+++ WebApiAndroidKlient/Ip.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace WebApiAndroidKlient
+{
+    class Ip
+    {
+        //public static String ip = "http://192.168.1.220:1508/api";
+        public static String ip = "http://10.0.2.2:1508/api";
+    }
+}
